69e43436072eccaa568cf856d52823b6b2f6556a
Nicu Barbaros <barbarosnicolae93@gmail.com>
2020-06-02 20:11:43 +0300

    👌 init commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,52 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import "../styles/home.scss";
-import AnimatedText from "../components/AnimatedText";
+import sampleData from "../utils/sampleData";
+import Item, { MediaItem } from "../components/Item";
 
-export default () => (
-  <div className="page-wrapper">
-    <div className="content-wrapper">
-      <div>
-        <h2>We are</h2>
-        <h2>Bread</h2>
-        <h2>Making Bakery</h2>
+// another example https://codedaily.io/tutorials/60/Create-a-useMousePosition-Hook-with-useEffect-and-useState-in-React
+const useMousePosition = () => {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
 
-        <p>full website coming soooon</p>
+  useEffect(() => {
+    const updateMousePosition = e => {
+      setMousePosition({ x: e.clientX, y: e.clientY });
+    };
+    window.addEventListener("mousemove", updateMousePosition);
+
+    return () => window.removeEventListener("mousemove", updateMousePosition);
+  }, []);
+
+  return mousePosition;
+};
+
+export default () => {
+  const { x, y } = useMousePosition();
+  const [activeIndex, setActiveIndex] = useState(-1);
+
+  return (
+    <div className="page-wrapper">
+      <div className="project-list">
+        {[...sampleData].map(({ title, mediaUrl, mediaType }, index) => (
+          <Item
+            key={index}
+            title={title}
+            index={index}
+            setActiveIndex={setActiveIndex}
+          />
+        ))}
+      </div>
+      <div className="project-media">
+        {sampleData.map(({ mediaUrl, mediaType }, index) => {
+          const isActive = index === activeIndex;
+          const xPos = isActive ? x : 0;
+          const yPos = isActive ? y : 0;
+          return (
+            <>
+              <MediaItem url={mediaUrl} x={xPos} y={yPos} active={isActive} />
+            </>
+          );
+        })}
       </div>
     </div>
-    <AnimatedText text="Bruttero" />
-  </div>
-);
+  );
+};
---
+++ src/utils/sampleData.js
@@ -0,0 +1,52 @@
+export default [
+  {
+    title: "Pineapple",
+    mediaUrl:
+      "https://images.unsplash.com/photo-1556690171-9f6645f4455e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=100",
+  },
+  {
+    title: "Banana x Art",
+    mediaUrl:
+      "https://images.unsplash.com/photo-1567341836769-242d01fe5c79?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1275&q=100",
+  },
+  {
+    title: "Green Peas",
+    mediaUrl:
+      "https://images.unsplash.com/photo-1590685006710-2b478c69b26b?ixlib=rb-1.2.1&auto=format&fit=crop&w=1300&q=100",
+  },
+  {
+    title: "Orange",
+    mediaUrl:
+      "https://images.unsplash.com/photo-1557800636-894a64c1696f?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=100",
+  },
+  {
+    title: "Strawberry",
+    mediaUrl:
+      "https://images.unsplash.com/photo-1539003425459-af206744e1db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=100",
+  },
+  {
+    title: "Avocado",
+    mediaUrl:
+      "https://images.unsplash.com/photo-1560155016-bd4879ae8f21?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=100",
+  },
+  {
+    title: "Lemon",
+    mediaUrl:
+      "https://images.unsplash.com/photo-1572635148818-ef6fd45eb394?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=100",
+  },
+  {
+    title: "Apple",
+    mediaUrl:
+      "https://images.unsplash.com/photo-1579613832125-5d34a13ffe2a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=100",
+  },
+  {
+    title: "Mango",
+    mediaUrl:
+      "https://images.unsplash.com/photo-1548587276-785018060579?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=100",
+  },
+  {
+    title: "Cherry",
+    mediaUrl:
+      "  https://images.unsplash.com/photo-1559181567-c3190ca9959b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=100",
+  },
+];
