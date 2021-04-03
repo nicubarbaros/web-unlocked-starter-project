60e73f73d63b81618696087ab2978e11ce850de8
Nicu Barbaros <barbarosnicolae93@gmail.com>
2021-04-03 22:35:06 +0300

    ♻️  Improve the started project

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-TODO
+#This is the starting project for Web Unlocked tutorials
+
+This project is bootstrapped by gatsby
+
+To run this started use
+`npm i` - to install dependencies
+`npm start` - to start local dev server
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,52 +1,11 @@
-import React, { useEffect, useState, useRef } from "react";
+import React from "react";
 import "../styles/home.scss";
-import sampleData from "../utils/sampleData";
-import Item, { MediaItem } from "../components/Item";
 
-// another example https://codedaily.io/tutorials/60/Create-a-useMousePosition-Hook-with-useEffect-and-useState-in-React
-const useMousePosition = () => {
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-
-  useEffect(() => {
-    const updateMousePosition = e => {
-      setMousePosition({ x: e.clientX, y: e.clientY });
-    };
-    window.addEventListener("mousemove", updateMousePosition);
-
-    return () => window.removeEventListener("mousemove", updateMousePosition);
-  }, []);
-
-  return mousePosition;
-};
-
-export default () => {
-  const { x, y } = useMousePosition();
-  const [activeIndex, setActiveIndex] = useState(-1);
-
+const Index = () => {
   return (
-    <div className="page-wrapper">
-      <div className="project-list">
-        {[...sampleData].map(({ title, mediaUrl, mediaType }, index) => (
-          <Item
-            key={index}
-            title={title}
-            index={index}
-            setActiveIndex={setActiveIndex}
-          />
-        ))}
-      </div>
-      <div className="project-media">
-        {sampleData.map(({ mediaUrl, mediaType }, index) => {
-          const isActive = index === activeIndex;
-          const xPos = isActive ? x : 0;
-          const yPos = isActive ? y : 0;
-          return (
-            <>
-              <MediaItem url={mediaUrl} x={xPos} y={yPos} active={isActive} />
-            </>
-          );
-        })}
-      </div>
+    <div>
+      <h1>This is the starting project for Web Unlocked tutorials</h1>
     </div>
   );
 };
+export default Index;
